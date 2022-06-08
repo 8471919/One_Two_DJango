06289adef58976a25b537a7427eb7c8412241f23
Michael <7471919@naver.com>
2022-06-09 01:57:43 +0900

    사용자의 일기만 가져올 수 있도록 수정

--- main_diary/views.py
+++ main_diary/views.py
@@ -1,4 +1,7 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
+from django.http import request
+from django.shortcuts import redirect
 from django.views.generic import ListView, DetailView
 
 from main_diary.models import Diary
@@ -8,5 +11,16 @@ class DiaryList(LoginRequiredMixin, ListView):
     model = Diary
     ordering = '-created_at'
 
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if (self.request.user):
+            qs = qs.filter(author=self.request.user.id)
+            return qs
+        else:
+            return redirect('/')
+
+
 class DiaryDetail(LoginRequiredMixin, DetailView):
     model = Diary
+
+
